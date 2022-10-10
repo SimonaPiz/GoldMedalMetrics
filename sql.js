b4270eaa43eabf570314b5a4f638b88d4bef6972
Simona Pizio <simona.pizio@gmail.com>
2022-10-10 16:27:45 +0200

    Create function mostSummerWins in sql.js

--- sql.js
+++ sql.js
@@ -35,7 +35,7 @@ won the most summer medals, along with the number of medals aliased to 'count'.
 */
 
 const mostSummerWins = country => {
-  return;
+  return ("SELECT year, COUNT(*) AS count FROM GoldMedal WHERE country LIKE '" + country + "' AND season LIKE 'summer' GROUP BY year ORDER BY 2 DESC;");
 };
 
 /*
